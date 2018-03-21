220a6318c84e3a7d65728141ca688a4837aa2a4c
Rafael lima <rafaellima1412@gmail.com>
2018-03-21 10:52:50 -0400

    teste

--- loader.js
+++ loader.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 
-server.use(bodyParser.urlencoded({extended: true}))
-sever.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true}))
+server.use(bodyParser.json())
 
 server.listen(port, function() {
   console.log(`BACKEND is runnning on port ${port}.`)
