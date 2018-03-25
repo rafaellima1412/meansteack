734c6979f601c8a88442fe692dc526dd48bd939e
Rafael lima <rafaellima1412@gmail.com>
2018-03-25 16:02:09 -0400

    atualizando o ciclo de faturamento

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -1,6 +1,7 @@
 const BillingCylce = require('./billingCycle')
 //API rest
 BillingCylce.methods(['get', 'post', 'put', 'delete'])
-BillingCylce.updateOptions({new: true, runValidators: false})
+//sempre que der update
+BillingCylce.updateOptions({new: true, runValidators: true})
 
 module.exports = BillingCylce
