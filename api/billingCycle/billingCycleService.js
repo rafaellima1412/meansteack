3c9b8e33775d7d77e7b3827617730651b3e25fdb
Rafael lima <rafaellima1412@gmail.com>
2018-03-25 18:15:03 -0400

    melhorando serviço back end

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -1,7 +1,17 @@
-const BillingCylce = require('./billingCycle')
-//API rest
-BillingCylce.methods(['get', 'post', 'put', 'delete'])
-//sempre que der update
-BillingCylce.updateOptions({new: true, runValidators: true})
+const BillingCycle = require('./billingCycle')
+//API rest responsavel pelo serviços de get,post,put e delete
+BillingCycle.methods(['get', 'post', 'put', 'delete'])
+//sempre que der update traz ultimo registro
+BillingCycle.updateOptions({new: true, runValidators: true})
+//devolve numeros de registros persistidos
+BillingCycle.route('count', function(req, res, next){
+  BillingCycle.count(function(error, value){
+    if(error){
+      res.status(500).json({error: [error]})
+    }else{
+      res.json({value})
+    }
+  })
+})
 
-module.exports = BillingCylce
+module.exports = BillingCycle
